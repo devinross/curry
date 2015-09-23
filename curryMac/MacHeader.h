e606ecea2a8419126796d436c455b0ab0df96824
Devin Ross <devinross@DevinsMacBook.local>
2015-09-23 19:47:24 -0400

    Adding mac framework

---
+++ curryMac/MacHeader.h
@@ -0,0 +1,27 @@
+//
+//  MacHeader.h
+//  curry
+//
+//  Created by Devin Ross on 9/23/15.
+//  Copyright © 2015 Devin Ross. All rights reserved.
+//
+
+#ifndef MacHeader_h
+#define MacHeader_h
+
+
+#import "TKFoundation.h"
+
+// Categories -> Foundation
+#import "NSObject+JSON.h"
+#import "NSObject+TKCategory.h"
+#import "NSString+TKCategory.h"
+#import "NSArray+TKCategory.h"
+#import "NSDate+TKCategory.h"
+#import "NSMutableArray+TKCategory.h"
+#import "NSBundle+TKCategory.h"
+#import "NSURLSession+TKCategory.h"
+#import "NSTimer+Blocks.h"
+
+
+#endif /* MacHeader_h */
---
+++ curryMac/curryMac.h
@@ -0,0 +1,20 @@
+//
+//  curryMac.h
+//  curryMac
+//
+//  Created by Devin Ross on 9/23/15.
+//  Copyright © 2015 Devin Ross. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for curryMac.
+FOUNDATION_EXPORT double curryMacVersionNumber;
+
+//! Project version string for curryMac.
+FOUNDATION_EXPORT const unsigned char curryMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <curryMac/PublicHeader.h>
+
+
+#import "MacHeader.h"
